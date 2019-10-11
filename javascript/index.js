3af39f303bb0fe9b45d06c942d3ffc2880ca8f0b
faris <faisabuarram@outlook.sa>
2019-10-11 21:16:23 +0300

    list of users design

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,34 @@
 
 
-arr = [];
-
+arr =Array();
+let info = {
+        name:""
+        , email: ""
+        , user_mojer:""
+        , user_role: ""
+        , bio: ""
+    };
 function setInformation() {
     let mojer = document.getElementById("userMajor");
-    let role = document.getElementById("userRole");
-    let info = {
-        name: document.getElementById("userName").value
-        , email: document.getElementById("userEmail").value
-        , user_mojer: mojer.options[mojer.selectedIndex].text
-        , user_role: role.options[role.selectedIndex].text
-        , bio: document.getElementById("userBio").value
-    };
+    let role = document.getElementById("userRole"); 
+info.name= document.getElementById("userName").value
+info.email= document.getElementById("userEmail").value
+info.user_mojer= mojer.options[mojer.selectedIndex].text
+info.user_role= role.options[role.selectedIndex].text
+info.bio=document.getElementById("userBio").value
 
 
 
-
-    save(info);
-
-
+    arr.push(info);
+   save(arr);
 }
-arr.push(get());
+
 function get() {
     const fromStorage = localStorage.getItem('arrlist');
     return fromStorage ? JSON.parse(fromStorage) : [];
 }
-function save(info) {
-    const jsonString = JSON.stringify(info);
+function save(arr) {
+    const jsonString = JSON.stringify(arr);
     localStorage.setItem('arrlist', jsonString);
 }
-console.log(arr);
+console.log(get());

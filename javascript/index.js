35bccdf148da739b8a46df7864fe112514df411d
faris <faisabuarram@outlook.sa>
2019-10-14 23:53:11 +0300

    the last

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,9 @@
-arr = new Array();
-arr = get();
+listOfUsers = new Array();// contain all users
 
+listOfUsers = get(); // get data fron local storage
+/*
+This class contain for information about all users
+*/
 class info {
     constructor(name, email, user_mojer, user_role, bio) {
         this.name = name;
@@ -13,8 +16,11 @@ class info {
     }
 
 }
+/*
+This function work when you click in the button(save) to save the users data and show it
+*/
 function formData(e) {
-    e.preventDefault();
+    e.preventDefault(); // to stop refresh
 
     let mojer = document.getElementById("userMajor");
     let role = document.getElementById("userRole");
@@ -23,19 +29,26 @@ function formData(e) {
     let user_mojer = mojer.options[mojer.selectedIndex].text;
     let user_role = role.options[role.selectedIndex].text;
     let bio = document.getElementById("userBio").value;
-    const newInfo = new info(name, email, user_mojer, user_role, bio);
-    let index=document.getElementById("add-index").value;
-    if (document.getElementById("add-to-bottom").checked == true)
-        arr.unshift(newInfo);
-    else if (index != "")
-        arr.splice( arr.length-index, 0, newInfo);
-    else
-        arr.push(newInfo);
-    
-    save(arr);
-    clear();
-    printDate()
+    const newInfo = new info(name, email, user_mojer, user_role, bio);//new class from class info
+    let index = document.getElementById("add-index").value;
+    if (uniqueEmail(email)) {
+        if (document.getElementById("add-to-bottom").checked == true)
+            listOfUsers.unshift(newInfo);
+        else if (index != "")
+            listOfUsers.splice(listOfUsers.length - index, 0, newInfo);
+        else
+            listOfUsers.push(newInfo);
+        localStorage.clear();
+        save(listOfUsers);
+        clear();// reset the form in the inputs text
+        printDate(listOfUsers);
+    }
+
+
 }
+/*
+This function to create div to display informatoin for the wall
+*/
 function shortStory(info) {
     return '<div class="short-story" id="short-story"><div class="icon"> <div class="line-inside-icon"></div></div> <div id="short-info" class="short-info"> <h1 id="nameofuser" class="name-of-user">' + info.name + '</h1>   <div class="info-specialization">  <h4 id="eemail" class="eemail">' + info.email + '</h4>  <div class="slash">/</div>   <h4 id="mojerofuser" class="mojer-of-user">' + info.user_mojer + '</h4>  <div class="slash">/</div>  <h4 id="roleofuser" class="role-of-user">' + info.user_role + '</h4>  </div><p id="bioo">' + info.bio + ' </p>  </div>';
 }
@@ -48,25 +61,140 @@ function clear() {
     document.getElementById("userRole").selectedIndex = "0";
     document.getElementById("userBio").value = null;
 }
+/*
+to get data from local storage
+*/
 function get() {
     const fromStorage = localStorage.getItem('arrlist');
     return fromStorage ? JSON.parse(fromStorage) : [];
 }
-function save(arr) {
-    const jsonString = JSON.stringify(arr);
+/*
+to save data to local storage
+*/
+function save(listOfUsers) {
+    const jsonString = JSON.stringify(listOfUsers);
     localStorage.setItem('arrlist', jsonString);
 }
-function printDate() {
+function printDate(array) {
     document.getElementById('information').innerHTML = "";
     var i;
-    for (i = arr.length - 1; i >= 0; i--) {
-        var createDiv = shortStory(arr[i]);
+    for (i = array.length - 1; i >= 0; i--) {
+        var createDiv = shortStory(array[i]);
         var htmlObject = document.createElement('div');
         htmlObject.innerHTML = createDiv;
         let obj = document.getElementById('information');
         obj.appendChild(htmlObject);
     }
-    document.getElementById('number-of-users').innerHTML = arr.length + " Items";
+    document.getElementById('number-of-users').innerHTML = listOfUsers.length + " Items";
 
 }
+function sorting() {
+    let arr1 = listOfUsers;
+    arr1.sort(function (a, b) {
+        var nameA = a.name.toUpperCase();
+        var nameB = b.name.toUpperCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
 
+
+        return 0;
+    });
+    let sortedChoice = document.getElementById('sort');
+    let choice = sortedChoice.options[sortedChoice.selectedIndex].value;
+    if (choice == 1)
+        printDate(arr1.reverse());
+    else if (choice == 2)
+        printDate(arr1);
+    else
+        printDate(arr);
+
+}
+function filterOfMojer() {
+    let arr2 = new Array();//array just contain Computer Science
+    let arr3 = new Array();//array just contain Computer Engineering
+    let arr4 = new Array();//array just contain Information Technology
+    let arr5 = new Array();//array just contain Information System
+    var i;
+    for (i = listOfUsers.length - 1; i >= 0; i--) {
+        if (listOfUsers[i].user_mojer == "Computer Science")
+            arr2.push(listOfUsers[i]);
+        if (listOfUsers[i].user_mojer == "Computer Engineering")
+            arr3.push(listOfUsers[i]);
+        if (listOfUsers[i].user_mojer == "Information Technology")
+            arr4.push(listOfUsers[i]);
+        if (listOfUsers[i].user_mojer == "Information System")
+            arr5.push(listOfUsers[i]);
+    }
+    console.log(arr2);
+    let sortedChoice = document.getElementById('major');
+    let choice = sortedChoice.options[sortedChoice.selectedIndex].value;
+    if (choice == 1) {
+        printDate(arr2);
+        document.getElementById('number-of-users').innerHTML = arr2.length + " Items";
+    }
+    else if (choice == 2) {
+        printDate(arr3);
+        document.getElementById('number-of-users').innerHTML = arr3.length + " Items";
+    }
+    else if (choice == 3) {
+        printDate(arr4);
+        document.getElementById('number-of-users').innerHTML = arr4.length + " Items";
+    }
+    else if (choice == 4) {
+        printDate(arr5);
+        document.getElementById('number-of-users').innerHTML = arr5.length + " Items";
+    }
+}
+function filterOfRole() {
+    let arr2 = new Array();//array just contain Front-End Developer
+    let arr3 = new Array();//array just contain Back-End Developer
+    let arr4 = new Array();//array just contain Full-Stack Develope
+    let arr5 = new Array();//array just contain UI/UX Designer
+    var i;
+    for (i = listOfUsers.length - 1; i >= 0; i--) {
+        if (listOfUsers[i].user_role == "Front-End Developer")
+            arr2.push(listOfUsers[i]);
+        if (listOfUsers[i].user_role == "Back-End Developer")
+            arr3.push(listOfUsers[i]);
+        if (listOfUsers[i].user_role == "Full-Stack Develope")
+            arr4.push(listOfUsers[i]);
+        if (listOfUsers[i].user_role == "UI/UX Designer")
+            arr5.push(listOfUsers[i]);
+    }
+    console.log(arr2);
+    let sortedChoice = document.getElementById('role-filter');
+    let choice = sortedChoice.options[sortedChoice.selectedIndex].value;
+    if (choice == 1) {
+        printDate(arr2);
+        document.getElementById('number-of-users').innerHTML = arr2.length + " Items";
+    }
+    else if (choice == 2) {
+        printDate(arr3);
+        document.getElementById('number-of-users').innerHTML = arr3.length + " Items";
+    }
+    else if (choice == 3) {
+        printDate(arr4);
+        document.getElementById('number-of-users').innerHTML = arr4.length + " Items";
+    }
+    else if (choice == 4) {
+        printDate(arr5);
+        document.getElementById('number-of-users').innerHTML = arr5.length + " Items";
+    }
+}
+
+
+function uniqueEmail(emailOfUser) {
+    var i;
+    for (i = listOfUsers.length - 1; i >= 0; i--) {
+        if (listOfUsers[i].email == emailOfUser)
+            return false;
+
+    }
+
+    return true;
+
+}

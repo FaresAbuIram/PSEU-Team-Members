091642c0e10480c124bb034b92d8597b6ad60bef
farisabu3ram <51198097+farisabu3ram@users.noreply.github.com>
2019-10-14 13:45:05 +0300

    display

--- javascript/index.js
+++ javascript/index.js
@@ -18,35 +18,19 @@ function formData(e) {
     info.user_mojer= mojer.options[mojer.selectedIndex].text
     info.user_role= role.options[role.selectedIndex].text
     info.bio=document.getElementById("userBio").value
-
-    
-
-    document.getElementById("nameofuser").innerHTML = info.name;
-    document.getElementById("eemail").innerHTML = info.email;
-    document.getElementById("mojerofuser").innerHTML = info.user_mojer;
-    document.getElementById("roleofuser").innerHTML = info.user_role;
-    document.getElementById("bioo").innerHTML = info.bio;
-
-
-   
+    console.log("obj.className");
+    var s =shortStory();
+    var htmlObject = document.createElement('div');
+    console.log(htmlObject);
+    htmlObject.innerHTML = s;
+    let obj = document.getElementById('information');
+    console.log(obj.id);
+   obj.appendChild(htmlObject);
+   //console.log( htmlObject);
+   clear();
 }
-
-   document.getElementById("hh").appendChild( document.getElementById('short-story'));
-   document.getElementById('short-story').style.display="flex";
-     clear();
-
-
-
-
-
-
-
-
-
-
-
-    
-
+function shortStory(){
+     return '<div class="short-story" id="short-story"><div class="icon"> <div class="line-inside-icon"></div></div> <div id="short-info" class="short-info"> <h1 id="nameofuser" class="name-of-user">'+info.name+'</h1>   <div class="info-specialization">  <h4 id="eemail" class="eemail">'+info.email+'</h4>  <div class="slash">/</div>   <h4 id="mojerofuser" class="mojer-of-user">'+ info.user_mojer+'</h4>  <div class="slash">/</div>  <h4 id="roleofuser" class="role-of-user">'+info.user_role+'</h4>  </div><p id="bioo">'+info.bio+' </p>  </div>';
 }
 
 function clear() {
